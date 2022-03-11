add0c051d8de92182a20532d08770a8363c707b9
Ruijian Cao <caoruijian@big.one>
2022-03-11 10:42:22 +0800

    fix

--- hello-world/hardhat.config.js
+++ hello-world/hardhat.config.js
@@ -3,6 +3,7 @@
  */
 require('dotenv').config();
 require("@nomiclabs/hardhat-ethers");
+require("@nomiclabs/hardhat-etherscan");
 
 const { API_URL, PRIVATE_KEY } = process.env;
 
@@ -16,4 +17,9 @@ module.exports = {
          accounts: [`0x${PRIVATE_KEY}`]
       }
    },
+   etherscan: {
+     // Your API key for Etherscan
+     // Obtain one at https://etherscan.io/
+     apiKey: process.env.ETHERSCAN_API_KEY
+   }
 }
